55079b09b61aa422327945580584f434b7f8dbde
SuPoPoo <supopo921@gmail.com>
2018-06-15 13:39:01 +0630

    addeds ex42

---
+++ ex42.py
@@ -0,0 +1,53 @@
+class Animal(object):
+    pass
+
+class Dog(Animal):
+
+    def__init__(self,name):
+        self.name=name
+
+class Cat(Animal):
+
+    def__init__(self,name):
+        self.name=name
+
+class Person(object):
+    
+    def__init__(self,name):
+
+        self.name=name
+        self.pet=None
+
+class Employee(Person):
+
+    def__init__(self,name,salary):
+
+        super(Employee,self).__init__(name)
+
+        self.salary=salary
+
+class Salmon(Fish):
+     pass
+
+ class Halibut(Fish):
+     pass
+
+ rover = Dog("Rover")
+
+ satan = Cat("Satan")
+
+ mary = Person("Mary")
+
+ mary.pet = satan
+
+ frank = Employee("Frank",120000)
+
+ frank.pet = rover
+
+ flipper = Fish()
+
+ crouse = Salmon()
+
+ harry = Halibut()
+
+
