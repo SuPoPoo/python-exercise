10fdc1decb82b7507c1f75ce388bb22bbb17c5a7
SuPoPoo <supopo921@gmail.com>
2018-06-22 11:25:03 +0530

    added ex44c

---
+++ ex44c.py
@@ -0,0 +1,19 @@
+class Parent(object):
+
+    def altered(self):
+        print("PARENT altered()")
+
+class Child(Parent):
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD , AFTER PARENT alteredd()")
+
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
+
