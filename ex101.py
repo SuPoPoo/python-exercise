aa2fa15d399c9e11ff1e0b375e1bf4c30979d710
SuPoPoo <39260987+SuPoPoo@users.noreply.github.com>
2018-06-26 08:41:03 +0530

    Update ex101.py

--- ex101.py
+++ ex101.py
@@ -1,15 +1,15 @@
-tabby_rad= "/tI'm tabbed in."
-persian_rad = "I'm split\non a line."
-backslash_rad = "I'm \\ a \\ dog/"
+tabby_rat= "/tI'm tabbed in."
+persian_rat = "I'm split\non a line."
+backslash_rat = "I'm \\ a \\ dog/"
 
-fat_rad ="""
+fat_rat ="""
 I'll do a list:
-\t* Rad food
+\t* Rat food
 \t* Fishies
-\t* Radnip\n\t* Grass
+\t* Ratnip\n\t* Grass
 """
 
-print(tabby_rad)
-print(persian_rad)
-print(backslash_rad)
-print(fat_rad)
+print(tabby_rat)
+print(persian_rat)
+print(backslash_rat)
+print(fat_rat)
