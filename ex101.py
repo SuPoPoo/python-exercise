78625611b24e0ee3c487f56c1f5ee755e27d5dc7
SuPoPoo <supopo921@gmail.com>
2018-06-26 10:06:49 +0530

    added ex101

--- ex101.py
+++ ex101.py
@@ -4,9 +4,9 @@ tabby_cat= "/tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat/"
 
-fat_cat ="""
+fat_dog ="""
 I'll do a list:
-\t* Dog food
+\t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
 """
